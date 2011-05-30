ad2fe5106d6d8fca9a7a27caefd548106c480391
Benn Huang <benn@allwinnertech.com>
2011-05-30 16:53:13 +0800

    add global build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+CUR_DIR=`pwd`
+
+# clean
+rm -rf ${CUR_DIR}/out/*
+mkdir ${CUR_DIR}/out
+
+# buildroot
+cd ${CUR_DIR}/buildroot && ./build.sh
+
+# build kernel
+export $PATH=${CUR_DIR}/buildroot/output/external-toolchain/bin:$PATH
+cd ${CUR_DIR}/linux-2.6.36 && ./build.sh kernel
+cd ${CUR_DIR}/linux-2.6.36 && ./build.sh modules
+
+
+# output
+cp -rf ${CUR_DIR}/linux-2.6.36/output/* ${CUR_DIR}/out/
+cp -rf ${CUR_DIR}/buildroot/output/images/* ${CUR_DIR}/out/
+
+
+
