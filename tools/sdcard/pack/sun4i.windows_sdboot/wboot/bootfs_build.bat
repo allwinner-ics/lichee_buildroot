66d1badd3ec5626ee5aee21a26a14f7c296cefeb
Benn Huang <benn@allwinnertech.com>
2011-06-06 15:13:47 +0800

    add pctools to buildroot

---
+++ tools/sdcard/pack/sun4i.windows_sdboot/wboot/bootfs_build.bat
@@ -0,0 +1,4 @@
+::==================================================
+:: build bootfs
+::==================================================
+..\pctools\fsbuild200\fsbuild.exe .\bootfs.ini ^ ..\efex\split_c43a2a92-a8ed-4f92-abdb-ca0d5d29666d.bin > bootfs.txt
