976c1191658539c6c3409b3db16c7642f6a881fa
Benn Huang <benn@allwinnertech.com>
2011-06-13 20:36:01 +0800

    update mali 3d packages

--- scripts/build.sh
+++ scripts/build.sh
@@ -23,7 +23,8 @@ rm -rf ${CUR_DIR}/out/rootfs
 
 # copy to out dir
 cp -f ${CUR_DIR}/linux-2.6.36/output/?Image ${CUR_DIR}/buildroot/output/target/
-cp -f ${CUR_DIR}/linux-2.6.36/output/lib/modules ${CUR_DIR}/buildroot/output/target/lib/modules
+mkdir -p ${CUR_DIR}/buildroot/output/target/lib/modules
+cp -rf ${CUR_DIR}/linux-2.6.36/output/lib/modules/* ${CUR_DIR}/buildroot/output/target/lib/modules/
 cp -f ${CUR_DIR}/linux-2.6.36/output/?Image ${CUR_DIR}/out/
 cp -rf ${CUR_DIR}/buildroot/output/target ${CUR_DIR}/out/rootfs
 
@@ -33,6 +34,8 @@ make rootfs-tar
 
 
 # copy to tools
+cp -f ${CUR_DIR}/linux-2.6.36/output/bImage ${CUR_DIR}/buildroot/tools/pack/sun4i_pack_ddr2_win/wboot/bootfs/linux/
+cp -f ${CUR_DIR}/linux-2.6.36/output/bImage ${CUR_DIR}/buildroot/tools/pack/sun4i_pack_ddr3_win/wboot/bootfs/linux/
 cp -f ${CUR_DIR}/buildroot/output/images/rootfs.ext2 ${CUR_DIR}/buildroot/tools/pack/sun4i_pack_ddr3_win/wboot/rootfs.fex
 cp -f ${CUR_DIR}/buildroot/output/images/rootfs.ext2 ${CUR_DIR}/buildroot/tools/pack/sun4i_pack_ddr2_win/wboot/rootfs.fex
 
