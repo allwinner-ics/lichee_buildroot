d53d80df58c1a81cb1ea559cfebc4691532ae767
Benn Huang <benn@allwinnertech.com>
2011-06-14 18:55:24 +0800

    update top_build.sh

--- scripts/top_build.sh
+++ scripts/top_build.sh
@@ -18,6 +18,52 @@ show_help()
 	printf "  ./build.sh -p sun4i -m kernel\n\n"
 }
 
+regen_rootfs()
+{
+	echo "Regenerating Rootfs..."
+	if [ -d ${CUR_DIR}/buildroot/output/target ]; then
+		mkdir -p ${CUR_DIR}/buildroot/output/target/lib/modules
+		cp -rf ${CUR_DIR}/linux-2.6.36/output/lib/modules/* \
+			${CUR_DIR}/buildroot/output/target/lib/modules/
+
+		cd ${CUR_DIR}/buildroot
+		make rootfs-ext2
+		make rootfs-tar
+		make rootfs-cpio
+
+	else
+		echo "Skip Regenerating Rootfs..."
+	fi
+}
+
+gen_output_sun3i()
+{
+	echo "output sun3i"
+}
+
+gen_output_sun4i()
+{
+	cp -v ${CUR_DIR}/linux-2.6.36/output/bImage \
+	${CUR_DIR}/buildroot/tools/pack/sun4i_pack_ddr2_win/wboot/bootfs/linux/
+	cp -v ${CUR_DIR}/linux-2.6.36/output/bImage \
+	${CUR_DIR}/buildroot/tools/pack/sun4i_pack_ddr3_win/wboot/bootfs/linux/
+	cp -v ${CUR_DIR}/buildroot/output/images/rootfs.ext2 \
+	${CUR_DIR}/buildroot/tools/pack/sun4i_pack_ddr2_win/wboot/rootfs.fex
+	cp -v ${CUR_DIR}/buildroot/output/images/rootfs.ext2 \
+	${CUR_DIR}/buildroot/tools/pack/sun4i_pack_ddr3_win/wboot/rootfs.fex
+
+	if [ -d "${CUR_DIR}/out" ]; then
+		mkdir -pv ${CUR_DIR}/out
+	fi
+
+	cp -v ${CUR_DIR}/buildroot/output/images/* ${CUR_DIR}/out/
+	cp -r ${CUR_DIR}/buildroot/output/target ${CUR_DIR}/out/rootfs
+	cp -r ${CUR_DIR}/linux-2.6.36/output/* ${CUR_DIR}/out/
+}
+
+
+
+
 while getopts hp:m: OPTION
 do
 	case $OPTION in
@@ -42,8 +88,13 @@ if [ "$MODULE" = buildroot ]; then
 	cd ${CUR_DIR}/buildroot && ./build.sh -p ${PLATFORM}
 elif [ "$MODULE" = kernel ]; then
 	cd ${CUR_DIR}/linux-2.6.36 && ./build.sh -p ${PLATFORM}
+	regen_rootfs
+	gen_output_${PLATFORM}
 else
 	cd ${CUR_DIR}/buildroot && ./build.sh -p ${PLATFORM}
 	cd ${CUR_DIR}/linux-2.6.36 && ./build.sh -p ${PLATFORM}
+	regen_rootfs
+	gen_output_${PLATFORM}
 fi
+
 
