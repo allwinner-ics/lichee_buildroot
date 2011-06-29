d46cc917d4130781ee47d5f271940a2214944d2b
Benn Huang <benn@allwinnertech.com>
2011-06-29 19:01:15 +0800

    update

--- target/sun4i.sh
+++ target/sun4i.sh
@@ -15,6 +15,16 @@ mount -t devpts none /dev/pts
 mknod /dev/mali c 230 0
 hostname sun4i
 
+export TSLIB_TSEVENTTYPE=H3600
+export TSLIB_CONSOLEDEVICE=none
+export TSLIB_FBDEVICE=/dev/fb0
+export TSLIB_TSDEVICE=/dev/input/event2
+
+export TSLIB_CALIBFILE=/etc/pointercal
+export TSLIB_CONFFILE=/etc/ts.conf
+export TSLIB_PLUGINDIR=/usr/lib/ts
+
+
 EOF
 
 touch output/target/etc/init.d/auto_config_network
@@ -27,15 +37,6 @@ MAC_ADDR="\`uuidgen |awk -F- '{print \$5}'|sed 's/../&:/g'|sed 's/\(.\)$//' |cut
 ifconfig eth0 hw ether "48\$MAC_ADDR"
 ifconfig lo 127.0.0.1
 udhcpc
-
-export TSLIB_TSEVENTTYPE=H3600
-export TSLIB_CONSOLEDEVICE=none
-export TSLIB_FBDEVICE=/dev/fb0
-export TSLIB_TSDEVICE=/dev/input/event2
-
-export TSLIB_CALIBFILE=/etc/pointercal
-export TSLIB_CONFFILE=/etc/ts.conf
-export TSLIB_PLUGINDIR=/usr/lib/ts
 
 EOF
 
