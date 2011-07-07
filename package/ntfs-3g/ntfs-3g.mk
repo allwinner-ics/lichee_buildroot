831ad5657fcd0a4ba584e7e26a4a8cca3b8db1af
Tom Cubie <tangliang@allwinnertech.com>
2011-07-07 10:45:09 +0800

    Update ntfs-3g to the newest version, remove ntfsprogs since the new ntfs-3g contain the ntfsprogs utilies

--- package/ntfs-3g/ntfs-3g.mk
+++ package/ntfs-3g/ntfs-3g.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-NTFS_3G_VERSION = 2010.10.2
+NTFS_3G_VERSION = 2011.4.12
 NTFS_3G_SOURCE = ntfs-3g-$(NTFS_3G_VERSION).tgz
 NTFS_3G_SITE = http://tuxera.com/opensource
 NTFS_3G_CONF_OPT = --disable-ldconfig --program-prefix=""
