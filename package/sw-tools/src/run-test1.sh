8284f11e503aefc23b9ae758f523680c22f34f5b
Benn Huang <benn@allwinnertech.com>
2011-08-22 12:46:12 +0800

    test: update run-test1.sh

--- package/sw-tools/src/run-test1.sh
+++ package/sw-tools/src/run-test1.sh
@@ -1,14 +1,43 @@
 #!/bin/ash
 
-export TSLIB_TSEVENTTYPE=H3600
-export TSLIB_CONSOLEDEVICE=none
-export TSLIB_FBDEVICE=/dev/fb0
-export TSLIB_TSDEVICE=/dev/input/event2
-export TSLIB_CALIBFILE=/etc/pointercal
-export TSLIB_CONFFILE=/etc/ts.conf
-export TSLIB_PLUGINDIR=/usr/lib/ts
-ts_calibrate
 
+run_sun4i_rts()
+{
+	_CTP_USED=`sw_ctl -f /dev/sw_ctl -m ctp_para -s ctp_used`
+
+	if [ "$CTP_USED" ! = "1" ]; then
+        echo "###############################TEST RTS###############################"
+        export TSLIB_TSEVENTTYPE=H3600
+        export TSLIB_CONSOLEDEVICE=none
+        export TSLIB_FBDEVICE=/dev/fb0
+        export TSLIB_TSDEVICE=/dev/input/event2
+        export TSLIB_CALIBFILE=/etc/pointercal
+        export TSLIB_CONFFILE=/etc/ts.conf
+        export TSLIB_PLUGINDIR=/usr/lib/ts
+        ts_calibrate
+	fi
+}
+
+CTP_USED=`sw_ctl -f /dev/sw_ctl -m ctp_para -s ctp_used`
+CTP_NAME=`sw_ctl -f /dev/sw_ctl -m ctp_para -s ctp_name`
+
+if [ "$CTP_USED" = "1" ]; then
+
+case $CTP_NAME in
+	ft5x_ts)
+	echo "Load ft5x touch screen driver"
+	(cd /lib/modules/`uname -r`/; insmod ft5x_ts.ko)
+	;;
+	Goodix-TS)
+	echo "Load goodix touch screen driver"
+        (cd /lib/modules/`uname -r`/; insmod goodix_touch.ko)
+	;;
+	*)
+	echo "Unknown touch screen: $CTP_NAME"
+	;;
+esac
+
+fi
 
 echo "After 3 seconds, input test start"
 sleep 3
@@ -25,17 +54,20 @@ for item in $(ls /sys/class/input |grep event)
 	  sun4i-keyboard)
 	  echo "#######################TEST KEY#########################"
 	  echo "INFO: $item, $DNAME"
-	  /test/td -t 30 -s 9 -c "/usr/bin/evtest /dev/input/$item"
+	  td -t 30 -s 9 -c "/usr/bin/evtest /dev/input/$item"
 	  ;;
 	  sun4i-ir)
 	  echo "#######################TEST IR#########################"
 	  echo "INFO: $item, $DNAME"
-	  /test/td -t 30 -s 9 -c "/usr/bin/evtest /dev/input/$item"
+	  td -t 30 -s 9 -c "/usr/bin/evtest /dev/input/$item"
 	  ;;
-	  *ts*)
+	  sun4i-ts)
+	  test-sun4i-rts
+	  ;;
+	  ft5x_ts)
 	  echo "#######################TEST TS#########################"
 	  echo "INFO: $item, $DNAME"
-	  /test/td -t 30 -s 9 -c "/usr/bin/evtest /dev/input/$item"
+	  td -t 30 -s 9 -c "/usr/bin/evtest /dev/input/$item"
 	  ;;
 	  *)
 	  echo "#######################Skip############################"
@@ -50,19 +82,19 @@ printf "\n\n#########################################################\n\n"
 printf "\n\n#### Test GPU 3D ####\n\n"
 insmod /lib/modules/`uname -r`/ump.ko
 insmod /lib/modules/`uname -r`/mali.ko
-/test/td -t 15 -s 9 -c "/test/gpu/lightshow/lightshow -w 800 -h 480"
+td -t 15 -s 9 -c "/test/gpu/lightshow/lightshow -w 800 -h 480"
 
 printf "\n\n#########################################################\n\n"
 
 printf "\n\n#### Test CedarX(LCD) ####\n\n"
-/test/td -t 30 -s 6 -c "/test/CedarXPlayerTest-1.4.1 /root/test1.mp4"
+td -t 30 -s 6 -c "/bin/CedarXPlayerTest /root/test1.mp4"
 
 printf "\n\n#########################################################\n\n"
 
 printf "\n\n#### Test CedarX(HDMI) ####\n\n"
 /test/display/fb_test.dat -o 4 9
 printf "\n\n#### Test CedarX ####\n\n"
-/test/td -t 30 -s 6 -c "/test/CedarXPlayerTest-1.4.1 /root/test1.mp4"
+td -t 30 -s 6 -c "/bin/CedarXPlayerTest /root/test1.mp4"
 /test/display/fb_test.dat -o 1 0
 
 printf "\n\n#########################################################\n\n"
@@ -74,11 +106,4 @@ ifconfig eth0 hw ether 3a:9d:33:46:d2:22
 ifconfig eth0 192.168.3.123
 ifconfig lo 127.0.0.1
 ping -c 10 192.168.3.1
-
-echo 7 > /proc/sys/kernel/printk
-
-
-
-
-
 
