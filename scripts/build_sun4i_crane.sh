dbf4400f1d8c21a8c1282d09e578c5ffcd5cb682
panlong <panlong@allwinnertech.com>
2011-10-21 15:23:15 +0800

    build: build_sun4i_crane.sh
    fix build toolstrain dispear

--- scripts/build_sun4i_crane.sh
+++ scripts/build_sun4i_crane.sh
@@ -10,4 +10,12 @@ set -e
 echo -e '\033[0;31;1m###################################\033[0m'
 echo -e '\033[0;31;1m#  skip make buildroot for crane  #\033[0m'
 echo -e '\033[0;31;1m###################################\033[0m'
+CUR_DIR=`pwd`
+export PATH=${CUR_DIR}/output/external-toolchain/bin:$PATH
+echo $CUR_DIR
+mkdir  output
+cd output
+tar -jxf ../dl/arm-2010.09-50-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2 
+mv arm-2010.09 external-toolchain 
+
 
