e13d4f1c479d2d946c23ad0f2ce1e662b73790f7
panlong <panlong@allwinnertech.com>
2011-11-02 19:54:09 +0800

    build: build_sun4i_crane.sh fix
    when toolstrain exist, scripts won`t uncompression again

--- scripts/build_sun4i_crane.sh
+++ scripts/build_sun4i_crane.sh
@@ -13,11 +13,11 @@ echo -e '\033[0;31;1m###################################\033[0m'
 CUR_DIR=`pwd`
 export PATH=${CUR_DIR}/output/external-toolchain/bin:$PATH
 echo $CUR_DIR
-if [  -e output ]; then
-rm -fr output
-fi
+if [ ! -e output ]; then
 mkdir  output
+fi
+if [ ! -e output/external-toolchain ];then
 cd output
 tar -jxf ../dl/arm-2010.09-50-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2 
 mv arm-2010.09 external-toolchain 
-
+fi
