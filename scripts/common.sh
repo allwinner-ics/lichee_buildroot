1c9a9f1071ac01f4d4f86bc602b8da5ab2e858a1
Benn Huang <benn@allwinnertech.com>
2011-11-03 17:28:52 +0800

    build: update common.sh to support kernel-3.0

--- scripts/common.sh
+++ scripts/common.sh
@@ -3,37 +3,62 @@ set -e
 
 PLATFORM=""
 MODULE=""
+BOARD="evb1.1"
 
 CUR_DIR=$PWD
 OUT_DIR=$CUR_DIR/out
-KERN_DIR=$CUR_DIR/linux-2.6.36
+KERN_VER=2.6.36
+KERN_DIR=$CUR_DIR/linux-${KERN_VER}
 KERN_OUT_DIR=$KERN_DIR/output
 BR_DIR=$CUR_DIR/buildroot
 BR_PACK_DIR=$BR_DIR/tools/pack
 BR_OUT_DIR=$BR_DIR/output
 
 
+#
+# VAR0: new KDIR
+#
+update_kdir()
+{
+	KERN_VER=$1
+	KERN_DIR=${CUR_DIR}/linux-${KERN_VER}
+	KERN_OUT_DIR=$KERN_DIR/output
+}
+
 show_help()
 {
-    printf "\nbuild.sh - Top Level Build Scripts\n\n"
-    echo "To pack sdcard, run \"touch .sdcard\" first, then run this script"
-    echo "================================================================="
-    echo "Valid Options:"
-    echo "  -h  Show help message"
-    echo "  -p <platform> platform"
-    printf "  -m <module> module, default to all\n"
-    printf "\nValid Platforms:\n"
-    printf "  sun4i        : full sun4i platform, including directfb, ...\n"
-    printf "  sun4i-lite   : lite sun4i platform, which has much less packages\n"
-    printf "  sun4i_crane  : for android, only have toolchain and kernel\n"
-    printf "  sun4i-debug  : for debug purpose\n\n"
-    printf "Valid Modules:\n"
-    printf "  kernel       : build linux kernel\n"
-    printf "  buildroot    : toolchain and package\n"
-    printf "  all          : kernel, buildroot, and auto packing\n\n"
-    printf "Examples:\n  ./build.sh -p sun4i\n"
-    printf "  ./build.sh -p sun4i -m buildroot\n"
-    printf "  ./build.sh -p sun4i -m kernel\n\n"
+printf "
+NAME
+    build - The top level build script for Lichee Linux BSP
+
+SYNOPSIS
+    build [-h] | [-p platform] [-b board_type] [-k kern_version] [-m module]
+
+OPTIONS
+    -h             Display help message
+    -p [platform]  platform, e.g. sun4i, sun4i-lite, sun4i-debug, sun4i_crane
+                   sun4i: full linux bsp
+                   sun4i-lite: linux bsp with less packages
+                   sun4i-debug: linux bsp for debug
+                   sun4i_crane: android kernel
+
+    -b [board]     Develoment board, e.g. evb1.1, evb1.2A, evb1.2B  [OPTIONAL]
+                   evb1.1(default): old EVB board, including EVB-V1.0, EVB-V1.1
+                   evb1.2A: RTP
+                   evb1.2B: CTP, 7' LCD
+
+    -k [kern_ver]  2.6.36(default), or 3.0                          [OPTIONAL]
+
+    -m [module]    Use this option when you dont want to build all. [OPTIONAL]
+                   e.g. kernel, buildroot, all(default)...
+
+Examples:
+    ./build.sh -p sun4i-lite
+    ./build.sh -p sun4i_crane
+    ./build.sh -p sun4i-lite -k 3.0 -b evb1.2B
+
+"
+
 }
 
 regen_rootfs()
@@ -41,7 +66,7 @@ regen_rootfs()
     if [ -d ${BR_OUT_DIR}/target ]; then
 		echo "Copy modules to target..."
         mkdir -p ${BR_OUT_DIR}/target/lib/modules
-        rm -rf ${BR_OUT_DIR}/target/lib/modules/2.6.36*
+        rm -rf ${BR_OUT_DIR}/target/lib/modules/${KERN_VER}*
         cp -rf ${KERN_OUT_DIR}/lib/modules/* ${BR_OUT_DIR}/target/lib/modules/
 
         if [ "$PLATFORM" = "sun4i-debug" ]; then
@@ -78,7 +103,8 @@ gen_output_generic()
 
 do_pack()
 {
-    (cd ${BR_PACK_DIR}; ./pack)
+    printf "Packing for $BOARD board\n"
+    (cd ${BR_PACK_DIR}; ./pack -B $BOARD)
 }
 
 gen_output_sun4i()
@@ -123,16 +149,23 @@ clean_output()
     rm -rf ${KERN_OUT_DIR}/*
 }
 
-while getopts hp:m: OPTION
+while getopts hb:p:m:k: OPTION
 do
     case $OPTION in
     h) show_help
+    exit 0
     ;;
     p) PLATFORM=$OPTARG
     ;;
     m) MODULE=$OPTARG
     ;;
+    k) KERN_VER=$OPTARG
+    update_kdir $KERN_VER
+    ;;
+    b) BOARD=$OPTARG
+    ;;
     *) show_help
+    exit 1
     ;;
 esac
 done
