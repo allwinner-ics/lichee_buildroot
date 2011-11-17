9a06ac30b67ce87159ebd709b399ca5df377ef8c
Benn Huang <benn@allwinnertech.com>
2011-11-17 14:29:38 +0800

    build: remove auto pack

--- scripts/common.sh
+++ scripts/common.sh
@@ -3,7 +3,6 @@ set -e
 
 PLATFORM=""
 MODULE=""
-BOARD="evb1.1"
 
 CUR_DIR=$PWD
 OUT_DIR=$CUR_DIR/out
@@ -11,14 +10,10 @@ KERN_VER=2.6.36
 KERN_DIR=$CUR_DIR/linux-${KERN_VER}
 KERN_OUT_DIR=$KERN_DIR/output
 BR_DIR=$CUR_DIR/buildroot
-BR_PACK_DIR=$BR_DIR/tools/pack
 BR_OUT_DIR=$BR_DIR/output
 U_BOOT_DIR=$CUR_DIR/u-boot
 
 
-#
-# VAR0: new KDIR
-#
 update_kdir()
 {
 	KERN_VER=$1
@@ -33,7 +28,7 @@ NAME
     build - The top level build script for Lichee Linux BSP
 
 SYNOPSIS
-    build [-h] | [-p platform] [-b board_type] [-k kern_version] [-m module]
+    build [-h] | [-p platform] [-k kern_version] [-m module]
 
 OPTIONS
     -h             Display help message
@@ -43,11 +38,6 @@ OPTIONS
                    sun4i-debug: linux bsp for debug
                    sun4i_crane: android kernel
 
-    -b [board]     Develoment board, e.g. evb1.1, evb1.2A, evb1.2B  [OPTIONAL]
-                   evb1.1(default): old EVB board, including EVB-V1.0, EVB-V1.1
-                   evb1.2A: RTP
-                   evb1.2B: CTP, 7' LCD
-
     -k [kern_ver]  2.6.36(default), or 3.0                          [OPTIONAL]
 
     -m [module]    Use this option when you dont want to build all. [OPTIONAL]
@@ -56,7 +46,7 @@ OPTIONS
 Examples:
     ./build.sh -p sun4i-lite
     ./build.sh -p sun4i_crane
-    ./build.sh -p sun4i-lite -k 3.0 -b evb1.2B
+    ./build.sh -p sun4i-lite
 
 "
 
@@ -64,147 +54,118 @@ Examples:
 
 regen_rootfs()
 {
-    if [ -d ${BR_OUT_DIR}/target ]; then
+	if [ -d ${BR_OUT_DIR}/target ]; then
 		echo "Copy modules to target..."
-        mkdir -p ${BR_OUT_DIR}/target/lib/modules
-        rm -rf ${BR_OUT_DIR}/target/lib/modules/${KERN_VER}*
-        cp -rf ${KERN_OUT_DIR}/lib/modules/* ${BR_OUT_DIR}/target/lib/modules/
+		mkdir -p ${BR_OUT_DIR}/target/lib/modules
+		rm -rf ${BR_OUT_DIR}/target/lib/modules/${KERN_VER}*
+		cp -rf ${KERN_OUT_DIR}/lib/modules/* ${BR_OUT_DIR}/target/lib/modules/
 
-        if [ "$PLATFORM" = "sun4i-debug" ]; then
-            cp -rf ${KERN_DIR}/vmlinux ${BR_OUT_DIR}/target
-        fi
+		if [ "$PLATFORM" = "sun4i-debug" ]; then
+			cp -rf ${KERN_DIR}/vmlinux ${BR_OUT_DIR}/target
+		fi
 	fi
 
 	if [ "$PLATFORM" != "sun4i_crane" ]; then
 		echo "Regenerating Rootfs..."
 		(cd ${BR_DIR}; make target-generic-getty-busybox; make target-finalize)
-        (cd ${BR_DIR};  make LICHEE_GEN_ROOTFS=y rootfs-ext4)
-    else
-        echo "Skip Regenerating Rootfs..."
-    fi
+        	(cd ${BR_DIR};  make LICHEE_GEN_ROOTFS=y rootfs-ext4)
+	else
+		echo "Skip Regenerating Rootfs..."
+	fi
 }
 
 gen_output_sun3i()
 {
-    echo "output sun3i"
+	echo "output sun3i"
 }
 
 gen_output_generic()
 {
-    cp -v ${KERN_OUT_DIR}/uImage ${BR_PACK_DIR}/wboot/kernel.fex
-    cp -v ${U_BOOT_DIR}/u-boot.bin ${BR_PACK_DIR}/wboot/bootfs/linux/
-    cp -v ${BR_OUT_DIR}/images/rootfs.ext4 ${BR_PACK_DIR}/wboot/rootfs.fex
+	if [ ! -d "${OUT_DIR}" ]; then
+		mkdir -pv ${OUT_DIR}
+	fi
 
-    if [ ! -d "${OUT_DIR}" ]; then
-        mkdir -pv ${OUT_DIR}
-    fi
-
-    cp -v ${BR_OUT_DIR}/images/* ${OUT_DIR}/
-    cp -r ${KERN_OUT_DIR}/* ${OUT_DIR}/
-}
-
-do_pack()
-{
-    PACK_CHIP=`echo $PLATFORM| awk -F[-_] '{print $1}'`
-    PACK_PLAT=`echo $PLATFORM| awk -F[-_] '{print $2}'`
-
-    case $PACK_PLAT in
-    crane*)
-        PACK_PLAT=crane
-        ;;
-    *)
-        PACK_PLAT=linux
-        ;;
-    esac
-
-    printf "Packing for $PACK_CHIP, $PACK_PLAT, $BOARD board\n"
-    (cd ${BR_PACK_DIR}; ./pack-new -c $PACK_CHIP -p $PACK_PLAT -b $BOARD)
+	cp -v ${BR_OUT_DIR}/images/* ${OUT_DIR}/
+	cp -r ${KERN_OUT_DIR}/* ${OUT_DIR}/
 }
 
 gen_output_sun4i()
 {
-    gen_output_generic
-    do_pack
+	gen_output_generic
 }
 
 gen_output_sun4i-lite()
 {
-    gen_output_generic
-    do_pack
+	gen_output_generic
 }
 
 gen_output_sun4i-debug()
 {
-    gen_output_generic
-    do_pack
+	gen_output_generic
 }
 
 gen_output_sun4i_crane()
 {
-    cp -v ${KERN_OUT_DIR}/bImage ${BR_PACK_DIR}/wboot/bootfs/linux/
+	if [ ! -d "${OUT_DIR}" ]; then
+		mkdir -pv ${OUT_DIR}
+	fi
 
-    if [ ! -d "${OUT_DIR}" ]; then
-        mkdir -pv ${OUT_DIR}
-    fi
+	if [ ! -d "${OUT_DIR}/android" ]; then
+		mkdir -p ${OUT_DIR}/android
+	fi
 
-    if [ ! -d "${OUT_DIR}/android" ]; then
-        mkdir -p ${OUT_DIR}/android
-    fi
-
-    cp -r ${KERN_OUT_DIR}/* ${OUT_DIR}/android/
-    mkdir -p ${OUT_DIR}/android/toolchain/
-    cp ${BR_DIR}/dl/arm-2010.09-50-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2 ${OUT_DIR}/android/toolchain/
+	cp -r ${KERN_OUT_DIR}/* ${OUT_DIR}/android/
+	mkdir -p ${OUT_DIR}/android/toolchain/
+	cp ${BR_DIR}/dl/arm-2010.09-50-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2 ${OUT_DIR}/android/toolchain/
 }
 
 clean_output()
 {
-    rm -rf ${OUT_DIR}/*
-    rm -rf ${BR_OUT_DIR}/images/*
-    rm -rf ${KERN_OUT_DIR}/*
+	rm -rf ${OUT_DIR}/*
+	rm -rf ${BR_OUT_DIR}/images/*
+	rm -rf ${KERN_OUT_DIR}/*
 }
 
-while getopts hb:p:m:k: OPTION
+while getopts hp:m:k: OPTION
 do
-    case $OPTION in
-    h) show_help
-    exit 0
-    ;;
-    p) PLATFORM=$OPTARG
-    ;;
-    m) MODULE=$OPTARG
-    ;;
-    k) KERN_VER=$OPTARG
-    update_kdir $KERN_VER
-    ;;
-    b) BOARD=$OPTARG
-    ;;
-    *) show_help
-    exit 1
-    ;;
+	case $OPTION in
+	h) show_help
+	exit 0
+	;;
+	p) PLATFORM=$OPTARG
+	;;
+	m) MODULE=$OPTARG
+	;;
+	k) KERN_VER=$OPTARG
+	update_kdir $KERN_VER
+	;;
+	*) show_help
+	exit 1
+	;;
 esac
 done
 
 if [ -z "$PLATFORM" ]; then
-    show_help
-    exit 1
+	show_help
+	exit 1
 fi
 
 clean_output
 
 if [ "$MODULE" = buildroot ]; then
-    cd ${BR_DIR} && ./build.sh -p ${PLATFORM}
+	cd ${BR_DIR} && ./build.sh -p ${PLATFORM}
 elif [ "$MODULE" = kernel ]; then
-    export PATH=${BR_OUT_DIR}/external-toolchain/bin:$PATH
-    cd ${KERN_DIR} && ./build.sh -p ${PLATFORM}
-    regen_rootfs
-    gen_output_${PLATFORM}
+	export PATH=${BR_OUT_DIR}/external-toolchain/bin:$PATH
+	cd ${KERN_DIR} && ./build.sh -p ${PLATFORM}
+	regen_rootfs
+	gen_output_${PLATFORM}
 else
-    cd ${BR_DIR} && ./build.sh -p ${PLATFORM}
-    export PATH=${BR_OUT_DIR}/external-toolchain/bin:$PATH
-    cd ${KERN_DIR} && ./build.sh -p ${PLATFORM}
-    cd ${U_BOOT_DIR} && make CROSS_COMPILE=arm-none-linux-gnueabi- aw1623
-    regen_rootfs
-    gen_output_${PLATFORM}
+	cd ${BR_DIR} && ./build.sh -p ${PLATFORM}
+	export PATH=${BR_OUT_DIR}/external-toolchain/bin:$PATH
+	cd ${KERN_DIR} && ./build.sh -p ${PLATFORM}
+	cd ${U_BOOT_DIR} && make CROSS_COMPILE=arm-none-linux-gnueabi- aw1623
+	regen_rootfs
+	gen_output_${PLATFORM}
 fi
 
 
