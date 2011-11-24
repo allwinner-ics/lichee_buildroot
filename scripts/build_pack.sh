58276ccf150f7205e2fde2961145a9b24c8feda4
Benn Huang <benn@allwinnertech.com>
2011-11-24 09:37:00 +0800

    build: change select

--- scripts/build_pack.sh
+++ scripts/build_pack.sh
@@ -2,130 +2,137 @@
 
 LICHEE_ROOT=$PWD
 PACK_ROOT=tools/pack
-INDEX=0
+TARGET_CHIP="sun4i"
+TARGET_PLATFORM="linux"
+TARGET_BOARD="evb"
+count=0
+
+select_chips()
+{
+    count=0
+
+    printf "All valid chips:\n"
+
+    for chip in $(cd $PACK_ROOT/configs/; find -mindepth 1 -maxdepth 1 -type d |sort); do
+        chips[$count]=`basename $PACK_ROOT/configs/$chip`
+        printf "$count. ${chips[$count]}\n"
+        let count=$count+1
+    done
+
+    while true; do
+        read -p "Please select a chip:"
+        RES=`expr match $REPLY "[0-9][0-9]*$"`
+        if [ "$RES" -le 0 ]; then
+            echo "please use index number"
+            continue
+        fi
+        if [ "$REPLY" -ge $count ]; then
+            echo "too big"
+            continue
+        fi
+        if [ "$REPLY" -lt "0" ]; then
+            echo "too small"
+            continue
+        fi
+        break
+    done
+
+    TARGET_CHIP=${chips[$REPLY]}
+}
+
+select_platform()
+{
+    count=0
+    chip=$1
+
+    printf "All valid platforms:\n"
+
+    for platform in $(cd $PACK_ROOT/configs/$chip/; find -mindepth 1 -maxdepth 1 -type d |sort); do
+        platforms[$count]=`basename $PACK_ROOT/configs/$chip/$platform`
+        printf "$count. ${platforms[$count]}\n"
+        let count=$count+1
+    done
+
+    while true; do
+        read -p "Please select a platform:"
+        RES=`expr match $REPLY "[0-9][0-9]*$"`
+        if [ "$RES" -le 0 ]; then
+            echo "please use index number"
+            continue
+        fi
+        if [ "$REPLY" -ge $count ]; then
+            echo "too big"
+            continue
+        fi
+        if [ "$REPLY" -lt "0" ]; then
+            echo "too small"
+            continue
+        fi
+        break
+    done
+
+    TARGET_PLATFORM=${platforms[$REPLY]}
+}
+
+select_boards()
+{
+    count=0
+    chip=$1
+    platform=$2
+
+    printf "All valid boards:\n"
+
+    for board in $(cd $PACK_ROOT/configs/$chip/$platform/; find -mindepth 1 -maxdepth 1 -type d |sort); do
+        boards[$count]=`basename $PACK_ROOT/configs/$chip/$platform/$board`
+        printf "$count. ${boards[$count]}\n"
+        let count=$count+1
+    done
+
+    while true; do
+        read -p "Please select a board:"
+        RES=`expr match $REPLY "[0-9][0-9]*$"`
+        if [ "$RES" -le 0 ]; then
+            echo "please use index number"
+            continue
+        fi
+        if [ "$REPLY" -ge $count ]; then
+            echo "too big"
+            continue
+        fi
+        if [ "$REPLY" -lt "0" ]; then
+            echo "too small"
+            continue
+        fi
+        break
+    done
+
+    TARGET_BOARD=${boards[$REPLY]}
+}
 
 printf "Start packing for Lichee system\n\n"
 
-printf "chips:\n"
-for chipp in $(find $PACK_ROOT/configs/ -mindepth 1 -maxdepth 1 -type d )
-do
-	let INDEX=$INDEX+1
-	chip=`basename $chipp`
-	printf "$INDEX. $chip\n"
-done
+select_chips
 
-CHIP_TYPE=not_exist
+select_platform $TARGET_CHIP
 
+select_boards $TARGET_CHIP $TARGET_PLATFORM
 
+echo "$TARGET_CHIP $TARGET_PLATFORM $TARGET_BOARD"
 
-while [ ! -d $PACK_ROOT/configs/$CHIP_TYPE ]; do
-	read -p "Please select a chip(name): "
-	expr $REPLY "+" 10 &> /dev/null
-	if [  $? -eq 0 ]
-	then
-	if [ $REPLY -eq 1 ]
-	then
-	CHIP_TYPE=sun4i
-	break
-	fi
-	if [ $REPLY -eq 2 ]
-        then
-	CHIP_TYPE=sun5i
-	break
-	fi
-	fi
-	CHIP_TYPE=$REPLY
-done
-printf "\n"
-echo $CHIP_TYPE
- 
-INDEX=0
-printf "platforms:\n"
-for platformp in $(find $PACK_ROOT/configs/$CHIP_TYPE/ -mindepth 1 -maxdepth 1 -type d)
-do
-	let INDEX=$INDEX+1
-	platform=`basename $PACK_ROOT/configs/$chip/$platformp`
-	printf "$INDEX. $platform\n"
-done
+if [ "$TARGET_PLATFORM" = "crane" ]; then
+    if [ -z "$CRAME_IMAGE_OUT" ]; then
+        echo "You need to export CRANE_IMAGE_OUT var to env"
+        exit 1
+    fi
 
-PLATFORM_TYPE=not_exist
-while [ ! -d $PACK_ROOT/configs/$CHIP_TYPE/$PLATFORM_TYPE ]; do
-        read -p "Please select a platform(name): "
-        	expr $REPLY "+" 10 &> /dev/null
-			if [  $? -eq 0 ]
-			then
-			if [ $REPLY -eq 1 ]
-			then
-			PLATFORM_TYPE=linux
-			break
-			fi
-			if [ $REPLY -eq 2 ]
-			then
-			PLATFORM_TYPE=crane
-			break
-			fi
-			fi
-	PLATFORM_TYPE=$REPLY
-done
-printf "\n"
-echo $PLATFORM_TYPE
+    if [ ! -f "$CRAME_IMAGE_OUT/system.img" ]; then
+        echo "You have wrong CRANE_IMAGE_OUT env"
+        exit 1
+    fi
+fi
 
-INDEX=0
-printf "boards:\n"
-for boardp in $(find $PACK_ROOT/configs/$CHIP_TYPE/$PLATFORM_TYPE -mindepth 1 -maxdepth 1 -type d)
-do
-	let INDEX=$INDEX+1
-	board=`basename $PACK_ROOT/configs/$chip/$platformp/$boardp`
-	printf "$INDEX. $board\n"
-done
-
-BOARD_TYPE=not_exist
-while [ ! -d $PACK_ROOT/configs/$CHIP_TYPE/$PLATFORM_TYPE/$BOARD_TYPE ]; do
-        read -p "Please select a board: "
-			
-			
-			
-		    expr $REPLY "+" 10 &> /dev/null
-			if [  $? -eq 0 ]
-			then
-			if [ $PLATFORM_TYPE == linux ]
-			then
-				case $REPLY in
-				1) BOARD_TYPE=evb-v12r ; break;;
-				2) BOARD_TYPE=default ; break ;;
-				3) BOARD_TYPE=tvdevb ; break ;;
-				4) BOARD_TYPE=evb ; break ;;
-				5) BOARD_TYPE=aino ;break ;;
-				6) BOARD_TYPE=evb-v13 ;break ;;
-				*) BOARD_TYPE=not_exist  ;;
-				
-				esac 
-				
-			elif [ $PLATFORM_TYPE == crane ]
-			then
-				case $REPLY in
-				1) BOARD_TYPE=onda_m702h6 ;break ;;
-				2) BOARD_TYPE=bk7011 ;break ;;
-				3) BOARD_TYPE=onda_n507h5 ;break ;;
-				4) BOARD_TYPE=evb-v12r ;break ;;
-				5) BOARD_TYPE=default ;break ;;
-				6) BOARD_TYPE=t780 ;break ;;
-				7) BOARD_TYPE=tvdevb ;break ;;
-				8) BOARD_TYPE=evb ;break ;;
-				9) BOARD_TYPE=aino ;break ;;
-				10) BOARD_TYPE=evb-v13 ;break ;;
-				*) BOARD_TYPE=not_exist ;;
-				
-				esac 
-			fi
-			fi
-		
-        BOARD_TYPE=$REPLY
-done
-printf "\n"
-echo $BOARD_TYPE
 cd $PACK_ROOT
-./pack -c $CHIP_TYPE -p $PLATFORM_TYPE -b $BOARD_TYPE
+./pack -c $TARGET_CHIP -p $TARGET_PLATFORM -b $TARGET_BOARD
 cd -
 
 
